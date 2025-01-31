e2b350ef996db693f1c8a787c9e1a131fb359986
Ibrahim <ibrahimmiishaq@gmail.com>
2025-01-30 17:47:23 -0800

    Updated Camera & Started Level Design

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -2,21 +2,28 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    [SerializeField] private Transform target = null;
+    [SerializeField] private Transform player1 = null;
+    [SerializeField] private Transform player2 = null;
+
     [SerializeField] private Vector3 offset = Vector3.zero;
-    [SerializeField] private float smoothStep = 10.0f;
+    [SerializeField] private float smoothStep = 15.0f;
 
     void Start()
     {
-        Vector3 targetPosition = target.position + offset;
+        // Initial camera setup
+        Vector3 targetPosition = (player1.position + player2.position) / 2 + offset;
         targetPosition.z = transform.position.z;
         transform.position = targetPosition;
     }
 
     void Update()
     {
-        Vector3 targetPositon = target.position + offset;
-        targetPositon.z = transform.position.z;
-        transform.position = Vector3.Lerp(transform.position, targetPositon, smoothStep * Time.deltaTime);
+        Vector3 midpoint = (player1.position + player2.position) / 2;
+
+        Vector3 targetPosition = midpoint + offset;
+
+        targetPosition.z = transform.position.z;
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothStep * Time.deltaTime);
     }
 }
